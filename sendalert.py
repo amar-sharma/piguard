c6fdda77bc2ff7646d223361cc44ab8f15b8ed4d
Amar Sharma <amarsharma.hacker@gmail.com>
2017-10-27 18:10:58 +0530

    Detection human

--- sendalert.py
+++ sendalert.py
@@ -1,8 +1,7 @@
-import picamera
 import time
 import datetime
 import smtplib
-import ConfigParse
+import ConfigParser
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
 from email import Encoders
@@ -10,20 +9,7 @@ from email import Encoders
 config = ConfigParser.ConfigParser()
 config.read('config')
 
-class CaptureImage:
-  def __init__(self):
-    self.camera = picamera.PiCamera()
-
-  def capture_image(self):
-    cam = self.camera
-    cam.start_preview()
-    time.sleep(5)
-    filename = '/tmp/' + datetime.datetime.now().strftime('%s') + '.jpg'
-    cam.capture(filename)
-    cam.stop_preview()
-    return filename
-
-class Email:
+class Alertall:
   def __init__(self):
     self.EMAIL_FROM = config.get('SMTP', 'MailFrom')
     self.EMAIL_TO = config.get('SMTP', 'MailTo')
@@ -32,27 +18,22 @@ class Email:
     self.SMTP_PASS = config.get('SMTP', 'Pass')
 
   def sendEmail(self, attachment):
+    print "Sending Email"
     SUBJECT = "Alert! Somebody Home"
     msg = MIMEMultipart()
     msg['Subject'] = SUBJECT
     msg['From'] = self.EMAIL_FROM
-    msg['To'] = ', '.join(self.EMAIL_TO)
+    msg['To'] = self.EMAIL_TO
 
     part = MIMEBase('application', "octet-stream")
     part.set_payload(open(attachment, "rb").read())
     Encoders.encode_base64(part)
 
     part.add_header('Content-Disposition', 'attachment; filename="'+attachment+'"')
-
     msg.attach(part)
 
     server = smtplib.SMTP()
     server.connect(self.SMTP_SERVER)
     server.login(self.SMTP_USER, self.SMTP_PASS)
     server.sendmail(self.EMAIL_FROM, self.EMAIL_TO, msg.as_string())
-
-cap = CaptureImage()
-image_file = cap.capture_image()
-
-emailit = Email()
-emailit.sendEmail(image_file)
+    print "Email Sent"
